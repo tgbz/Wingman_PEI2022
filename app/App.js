eb2e022ef27f02519e0c9b7fc1891e4ede45ac65
tgbz <tgomesbz@gmail.com>
2022-11-12 15:25:31 +0000

    update

--- app/App.js
+++ app/App.js
@@ -1,6 +1,3 @@
-
-
-
 /* 
 View é um componente que serve para renderizar elementos no ecrâ, invocando outros componentes React Native.
 Text é um componente que serve para renderizar texto no ecrâ.
@@ -12,15 +9,12 @@ Stylesheet é um componente que serve para definir estilos para os componentes R
 
 */
 
-
-
-
 //Function Component, também podemos usar Class componentes
 /* 
 Se está View for para IOS, esta vai estar mapeada para UIView (NATIVO IOS),
 se for para Android, esta vai estar mapeada para View.
 
-*//*
+*/ /*
 import AppStack from './src/Navigators/AppNavigator';
 import AuthStack from './src/Navigators/AuthNavigator';
 import { NavigationContainer } from '@react-navigation/native';
@@ -45,31 +39,31 @@ const App = () => {
 export default App;
 */
 
-import AppStack from './src/Navigators/AppNavigator';
-import AuthStack from './src/Navigators/AuthNavigator';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { NavigationContainer } from '@react-navigation/native';
-import React, { useState, useEffect } from 'react';
-import { serverURL } from './src/config/hosts';
-import axios from 'axios';
-import AuthContext from './src/context/AuthProvider';
+import AppStack from "./src/navigators/AppNavigator";
+import AuthStack from "./src/navigators/AuthNavigator";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { NavigationContainer } from "@react-navigation/native";
+import React, { useState, useEffect } from "react";
+import { serverURL } from "./src/config/hosts";
+import axios from "axios";
+import AuthContext from "./src/context/AuthProvider";
 
 const App = () => {
   const [state, dispatch] = React.useReducer(
     (prevState, action) => {
       switch (action.type) {
-        case 'RESTORE_TOKEN':
+        case "RESTORE_TOKEN":
           return {
             ...prevState,
             userToken: action.userToken,
           };
-        case 'SIGN_IN':
+        case "SIGN_IN":
           return {
             ...prevState,
             isSignout: false,
             userToken: action.userToken,
           };
-        case 'SIGN_OUT':
+        case "SIGN_OUT":
           return {
             ...prevState,
             isSignout: true,
@@ -86,70 +80,71 @@ const App = () => {
   React.useEffect(() => {
     // Fetch the token from storage then navigate to our appropriate place
     const getToken = async () => {
-            try{
-              const jsonValue = await AsyncStorage.getItem('userToken')
-              if( jsonValue != null ){
-                console.log(JSON.parse(jsonValue))
-                dispatch({ type: 'RESTORE_TOKEN', userToken: JSON.parse(jsonValue)});
-              }     
-            }
-            catch(e){
-              console.log(e)
-            } 
-          }
-          getToken()
-          console.log(state)
+      try {
+        const jsonValue = await AsyncStorage.getItem("userToken");
+        if (jsonValue != null) {
+          console.log(JSON.parse(jsonValue));
+          dispatch({ type: "RESTORE_TOKEN", token: JSON.parse(jsonValue) });
+        }
+      } catch (e) {
+        console.log(e);
+      }
+      //dispatch({ type: "RESTORE_TOKEN", userToken: userToken });
+    };
+    getToken();
+    console.log(state);
   }, []);
 
   const authContext = React.useMemo(
     () => ({
-      signIn: async (email,password) => {
-        console.log(serverURL+"/users/login")
-        await axios
-        .post(serverURL+"/users/login", {
-            username: email,
+      signIn: async (email, password) => {
+        console.log(serverURL + "/users/login");
+        //sign in request with fetch,
+        //if success, save token in async storage and dispatch SIGN_IN action
+        //if error, show error message
+        try {
+          const response = await axios.post(serverURL + "/users/login", {
+            email: email,
             password: password,
-        })
-        .then((response) => {
-            if(response.data) {
-            console.log(response.data)
-            AsyncStorage.setItem('userToken', JSON.stringify(response.data));
-            dispatch({ type: 'SIGN_IN', userToken: JSON.stringify(response.data) });
-
-            }else{
-            alert(response.status);
-            }
-        })
-        .catch((error) => {
-            console.log(error);
-            alert("De momento não é possível processar a autenticação!");
-        });
+          });
+          console.log(response.data);
+          if (response.data.success) {
+            await AsyncStorage.setItem(
+              "userToken",
+              JSON.stringify(response.data.token)
+            );
+            dispatch({ type: "SIGN_IN", token: response.data.token });
+          } else {
+            alert(response.data.message);
+          }
+        } catch (e) {
+          console.log("ERRO")
+          console.log(e);
+        }
       },
-      signOut: () =>{
-        AsyncStorage.removeItem('userToken');
-        dispatch({ type: 'SIGN_OUT' })
-      } ,
+      signOut: () => {
+        AsyncStorage.removeItem("userToken");
+        dispatch({ type: "SIGN_OUT" });
+      },
       signUp: async (data) => {
         // In a production app, we need to send user data to server and get a token
         // We will also need to handle errors if sign up failed
         // After getting token, we need to persist the token using `SecureStore`
         // In the example, we'll use a dummy token
 
-        dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
+        dispatch({ type: "SIGN_IN", token: "dummy-auth-token" });
       },
     }),
     []
   );
-  
+
   return (
     <AuthContext.Provider value={authContext}>
-      <NavigationContainer >
-          {state.userToken==null ? (<AuthStack/>) : (<AppStack/>)}
+      <NavigationContainer>
+        {state.userToken == null ? <AuthStack /> : <AppStack />}
       </NavigationContainer>
     </AuthContext.Provider>
   );
 };
 
-
 export default App;
-

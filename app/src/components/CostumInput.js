617679e773642cc38e3828c913e71de793ba38ba
saracqueiros <48985082+saracqueiros@users.noreply.github.com>
2022-11-19 13:31:25 +0000

    Update CostumInput.js

--- app/src/components/CostumInput.js
+++ app/src/components/CostumInput.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput, useWindowDimensions } from "react-native";
 import { useState } from "react";
+import {FONTS,COLORS, SHADOWS, SIZES } from '../constants'
 
 
-const CostumInput = ({value, setValue, placeholder, secureTextEntry}) => {
+
+const CostumInput = ({value, setValue, placeholder, secureTextEntry, bgColor = COLORS.white, border= COLORS.white, placeholderColor=COLORS.placeholder}) => {
+    const {width} = useWindowDimensions();
     return (
-    <View style={styles.container}>
+    <View style={[styles.container,  {width: width *0.85 }, {backgroundColor: bgColor}, {borderColor: border}]}>
         <TextInput 
         value={value}
         onChangeText={setValue}
         placeholder={placeholder} 
+        placeholderTextColor = {placeholderColor}
         style={styles.input}
-        secureTextEntry={secureTextEntry}/>
+        secureTextEntry={secureTextEntry}
+        />
     </View>
 );
 };
@@ -19,13 +24,12 @@ const CostumInput = ({value, setValue, placeholder, secureTextEntry}) => {
 
 const styles = StyleSheet.create({
     container:{
-        backgroundColor: 'white',
-        width:'90%',
-        borderColor: 'white',
         borderRadius: 5,
         paddingHorizontal: 10,
         paddingVertical: 15,
-        marginVertical: 12
+        marginVertical: 12,
+        borderWidth: 1,
+        borderColor: COLORS.white
     },
     input: {},
    

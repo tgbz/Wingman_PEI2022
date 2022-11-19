9dfcb7774bf9de01ffcb6466d6ab4e3b47426d94
angelicasc <angelicasc22@hotmail.com>
2022-11-19 14:37:55 +0000

    Update

--- app/src/config/hosts.js
+++ app/src/config/hosts.js
@@ -5,9 +5,9 @@ let serverURL = "";
 if (Platform.OS == 'android') {
     serverURL = 'http://10.0.2.2:3000';
 } else if (Platform.OS == 'ios') {
-    serverURL = 'http://10.0.2.2:3000';
+    serverURL = 'http://4f1b-2001-8a0-f3f5-b100-c5f1-1232-1eb4-80a2.ngrok.io';
 } else {
-    serverURL = 'http://localhost:3000';
+    serverURL = 'http://localhost:3000';s
 }
 
 export { serverURL };
--- app/src/navigators/AppNavigator.js
+++ app/src/navigators/AppNavigator.js
@@ -1,16 +1,16 @@
 import * as React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from "../screens/HomeScreen.js"
-
+import ProfileScreen from "../screens/ProfileScreen.js"
 const Stack = createNativeStackNavigator();
 
 const AppStack = (name) => {
   return (
       <Stack.Navigator >
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-        />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        {/* Dont show stack navigator on top /> */}
+        <Stack.Screen name="Profile" component={ProfileScreen} options={{headerShown: false}}  />
+        
       </Stack.Navigator>
   );
 };

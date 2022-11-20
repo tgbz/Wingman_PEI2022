cdca97b6959d50c2a545a0fd406eb4b7e3a7d50c
Diana Filipa de Sousa Ferreira <dianafferreira0@gmail.com>
2022-11-20 03:33:26 +0000

    Category Controller and Routes

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -4,7 +4,11 @@ var Categories = module.exports;
 // Get todas as categorias e o total gasto em cada
 Categories.getCategory = function(id) {
     return new Promise(function(resolve,reject){
-        sql.query(`SELECT * FROM categories where idUser=?`,
+        sql.query(`select category.idUser, category.idcategory, category.name, sum(subcategory.plafond) as total_plafond_category, sum(subcategory.total_spent) as total_spent_category
+                   from category
+                   inner join subcategory on category.idcategory = subcategory.idcategory
+                   where category.idUser = ? 
+                   group by category.idcategory`,
         id ,function(err,res){
             if(err) {
                 console.log("error: ", err);
--- server/routes/categories.js
+++ server/routes/categories.js
@@ -3,8 +3,10 @@ var router = express.Router();
 var Categories = require("../controllers/category");
 
 // Get todas as categorias e o total gasto em cada
-
-
-
+router.get("/userCategory/:id", function (req, res){
+    Categories.getCategory(req.params.id)
+    .then( dados => res.jsonp(dados))
+    .catch(erro => res.status(500).jsonp(erro))
+  }) 
 
 module.exports = router;

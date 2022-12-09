e49fa84bd87917ce38d92a850aa59dcb54db2c1a
angelicasc <angelicasc22@hotmail.com>
2022-12-09 16:26:55 +0000

    Update hosts.js

--- app/src/config/hosts.js
+++ app/src/config/hosts.js
@@ -5,7 +5,7 @@ let serverURL = "";
 if (Platform.OS == 'android') {
     serverURL = 'https://180a-2001-8a0-ff73-4200-199d-2023-73e5-818c.eu.ngrok.io';
 } else if (Platform.OS == 'ios') {
-    serverURL = 'https://180a-2001-8a0-ff73-4200-199d-2023-73e5-818c.eu.ngrok.io';
+    serverURL = 'https://ed86-2001-8a0-f3f5-b100-c8aa-9198-adfc-4aca.ngrok.io';
 } else {
     serverURL = 'http://localhost:3000';
 }

a5c820a7bf8e12d9ab47605057e3762e0414368e
saracqueiros <saracristina-2000@hotmail.com>
2023-01-04 17:28:41 +0000

    A ver se é desta esteves

--- app/src/config/hosts.js
+++ app/src/config/hosts.js
@@ -3,9 +3,9 @@ import { Platform } from 'react-native';
 let serverURL = "";
 
 if (Platform.OS == 'android') {
-    serverURL = 'https://8b6e-2001-8a0-ff73-4200-60a7-4fa4-3047-2538.eu.ngrok.io';
+    serverURL = 'https://e10b-2001-8a0-ff73-4200-e5c6-6e1a-678b-34c1.eu.ngrok.io';
 } else if (Platform.OS == 'ios') {
-    serverURL = 'https://8b6e-2001-8a0-ff73-4200-60a7-4fa4-3047-2538.eu.ngrok.io';
+    serverURL = 'https://e10b-2001-8a0-ff73-4200-e5c6-6e1a-678b-34c1.eu.ngrok.io';
 } else {
     serverURL = 'http://localhost:3000';
 }

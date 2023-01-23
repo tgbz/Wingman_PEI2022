77f1129a4f6368d80869cb75ddb94785afbeea09
Eduardo Araujo <eduardojafaraujo@gmail.com>
2023-01-23 17:20:40 +0000

    ocrtotalOK

--- OCR/test.py
+++ OCR/test.py
@@ -1,15 +1,4 @@
-import zipfile
-import os
-import shutil
 
+from difflib import SequenceMatcher
 
-#shutil.make_archive('file', format='zip', root_dir='\\receivedFiles')
-
-fileDir = os.getcwd() + "\\receivedFiles\\"
-r = shutil.unpack_archive(os.getcwd() +"\\file.zip", os.getcwd() + "\\receivedFiles")
-files = os.listdir(fileDir)
-files = [fileDir+f for f in files if os.path.isfile(fileDir+f)]
-print(files)
-
-#with zipfile.ZipFile("file.zip", 'r') as zip_ref:
- #   zip_ref.extractall(os.getcwd() + "\\receivedFiles")
+print(SequenceMatcher(None,"total a pagar","total a pagar").ratio()) 

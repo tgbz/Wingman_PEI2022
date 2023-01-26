9b6e405f7f594f8fd20a482a9765195366528b68
tgbz <tgomesbz@gmail.com>
2023-01-26 17:06:57 +0000

    tweaks front end

--- app/src/config/hosts.js
+++ app/src/config/hosts.js
@@ -1,12 +1,5 @@
 import { Platform } from "react-native";
 
-let serverURL = "";
+let serverURL = "https://4972-2001-818-e7a8-c500-a0e8-b383-215a-5cee.eu.ngrok.io"
 
-if (Platform.OS == "android") {
-  serverURL = "https://a757-2001-8a0-ff73-4200-3128-44f9-c9dd-43c5.eu.ngrok.io";
-} else if (Platform.OS == "ios") {
-  serverURL = "https://a757-2001-8a0-ff73-4200-3128-44f9-c9dd-43c5.eu.ngrok.io";
-} else {
-  serverURL = "http://localhost:3000";
-}
 export { serverURL };

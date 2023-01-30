5f0058c5d6cb8e3a045accf8527bddb7e46373cd
saracqueiros <saracristina-2000@hotmail.com>
2023-01-30 12:39:23 +0000

    ocr c qtds

--- app/src/config/hosts.js
+++ app/src/config/hosts.js
@@ -5,7 +5,7 @@ let serverURL = "https://30fe-2001-818-e7a8-c500-551d-4197-9227-2dd7.eu.ngrok.io
 if (Platform.OS == "android") {
   serverURL = "https://30fe-2001-818-e7a8-c500-551d-4197-9227-2dd7.eu.ngrok.io";
 } else if (Platform.OS == "ios") {
-  serverURL = "https://30fe-2001-818-e7a8-c500-551d-4197-9227-2dd7.eu.ngrok.io";
+  serverURL = "https://cd5b-2001-8a0-ff73-4200-40a5-acfd-bb75-d916.eu.ngrok.io";
 } else {
   serverURL = "http://localhost:3000";
 }

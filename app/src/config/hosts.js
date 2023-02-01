fd28022316abde8a6dafae0ec395ac8fd6781ba1
saracqueiros <saracristina-2000@hotmail.com>
2023-02-01 16:11:02 +0000

    fixes

--- app/src/config/hosts.js
+++ app/src/config/hosts.js
@@ -3,9 +3,9 @@ import { Platform } from "react-native";
 let serverURL = "https://c7e8-2001-8a0-f3f5-b100-943a-a533-1fb9-2f55.ngrok.io"
 
 if (Platform.OS == "android") {
-  serverURL = "https://30fe-2001-818-e7a8-c500-551d-4197-9227-2dd7.eu.ngrok.io";
+  serverURL = "https://7ad7-2001-8a0-ff73-4200-1980-3344-8ccc-3c1.eu.ngrok.io";
 } else if (Platform.OS == "ios") {
-  serverURL = "https://c7e8-2001-8a0-f3f5-b100-943a-a533-1fb9-2f55.ngrok.io";
+  serverURL = "https://7ad7-2001-8a0-ff73-4200-1980-3344-8ccc-3c1.eu.ngrok.io";
 } else {
   serverURL = "http://localhost:3000";
 }
